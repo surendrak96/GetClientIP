e0db57f4cc7f25afea430ef58c8b84191cb6db3c
surendra <surendrak96@gmail.com>
2016-05-07 19:51:33 +0530

    commited

---
+++ index.php
@@ -0,0 +1,57 @@
+
+<?php
+// this method doesnt work in localhost 
+
+function get_client_ip_env() {
+//get the client ip address using get_client_ip_env function
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+ echo 'Your IP address (using get_client_ip_env function) is ' . get_client_ip_env() . '<br />';
+
+ 
+function get_client_ip_server() {
+// get the client ip address using get_client_ip_server function)
+
+    $ipaddress = '';
+
+    if ($_SERVER['HTTP_CLIENT_IP'])
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if($_SERVER['HTTP_X_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if($_SERVER['HTTP_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if($_SERVER['HTTP_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if($_SERVER['REMOTE_ADDR'])
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+
+    return $ipaddress;
+}
+echo 'Your IP address (using get_client_ip_server function) is ' . get_client_ip_server() . '<br />';
+
+
+// Get the client ip address $_SERVER['REMOTE_ADDR'] in single line 
+$ipaddress = $_SERVER['REMOTE_ADDR'];
+echo 'Your IP address (using $_SERVER[\'REMOTE_ADDR\']) is ' . $ipaddress . '<br />';
+
+
+?>
